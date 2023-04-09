876369e3d68e7450a241fff6d6dd45550642da1c
akashislam1 <m.naimul110@gmail.com>
2023-04-09 22:59:12 +0600

    added on Header name, link, button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="shadow-md rounded-md p-3  flex justify-between items-center">
+      <div>
+        <Link to="/" className="inline-flex items-center">
+          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 ">
+            Today'S JOB
+          </span>
+        </Link>
+      </div>
+      <div>
+        <ul className="flex gap-4 font-semibold">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/statistics">Statistics</Link>
+          </li>
+          <li>
+            <Link to="/appliedJob">Applied Jobs</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <button className="p-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-md font-bold">
+          Start Applying
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header/Header";
+
+const Home = () => {
+  return (
+    <div className="w-full lg:w-5/6 mx-auto">
+      <Header></Header>
+    </div>
+  );
+};
+
+export default Home;
