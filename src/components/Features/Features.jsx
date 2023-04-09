69f301631286e1b4425ce32d032a22fe81c7f375
akashislam1 <m.naimul110@gmail.com>
2023-04-10 01:42:46 +0600

    added handleShowAll button  for see all job

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -3,6 +3,10 @@ import Feature from "./Feature";
 
 const Features = () => {
   const [features, setFeatures] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+  const handleShowAll = () => {
+    setShowAll(true);
+  };
 
   useEffect(() => {
     fetch("../../../public/data.json")
@@ -10,20 +14,29 @@ const Features = () => {
       .then((data) => setFeatures(data));
   }, []);
   return (
-    <div className=" my-28 p-1">
-      <div className=" lg:text-center">
-        <h1 className="text-2xl lg:text-5xl font-bold">Featured Jobs</h1>
-        <p className="text-primary mt-3">
-          Explore thousands of job opportunities with all the information you
-          need. Its your future
-        </p>
+    <>
+      <div className=" my-28 p-1">
+        <div className=" lg:text-center">
+          <h1 className="text-2xl lg:text-5xl font-bold">Featured Jobs</h1>
+          <p className="text-primary mt-3">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future
+          </p>
+        </div>
+        <div className="grid lg:grid-cols-2 gap-5 ">
+          {features?.slice(0, showAll ? 10 : 4).map((feature) => (
+            <Feature key={feature.id} feature={feature}></Feature>
+          ))}
+        </div>
+        {!showAll && (
+          <div className="text-center mt-5">
+            <button onClick={handleShowAll} className="btn-primary">
+              See All Jobs
+            </button>
+          </div>
+        )}
       </div>
-      <div className="grid lg:grid-cols-2 gap-5 ">
-        {features.map((feature) => (
-          <Feature key={feature.id} feature={feature}></Feature>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
